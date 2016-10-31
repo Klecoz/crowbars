10525bd5b67071a761a6b9eea86a4a32c051692c
Arsenio Colon <arseniocolon@gmail.com>
2016-10-31 18:39:27 -0400

    first commit

---
+++ Assets/scripts/generateCrowBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generateCrowBar : MonoBehaviour {
+
+    public GameObject crowbar;
+
+    public float spawnTime = 3f;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("spawnCrowbar", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void spawnCrowbar()
+    {
+        var newCrowbar = GameObject.Instantiate(crowbar, new Vector3(Random.Range(-2, 2), 2, 8), Quaternion.identity);
+    }
+}
---
+++ Assets/scripts/moveCrowBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCrowBar : MonoBehaviour {
+
+    Vector3 movement = new Vector3 (0,0,-2);
+	// Use this for initialization
+	void Start () {
+	   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(movement * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.right * 1, Space.Self);
+	}
+}
